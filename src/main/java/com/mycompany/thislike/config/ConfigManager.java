2f9a68cf0bdfded3a3d6fb314bb104a63b61d09b
Kumaisu <sugi.chama.1119@ezweb.ne.jp>
2020-05-04 08:43:59 +0900

    Change Status content ・ステータス表示内容の変更

--- src/main/java/com/mycompany/thislike/config/ConfigManager.java
+++ src/main/java/com/mycompany/thislike/config/ConfigManager.java
@@ -85,35 +85,36 @@ public class ConfigManager {
         Tools.Prt( p, ChatColor.WHITE + "Degub Mode  : " + ChatColor.YELLOW + Tools.consoleFlag.get( programCode ).toString(), programCode );
         Tools.Prt( p, ChatColor.WHITE + "Mysql       : " + ChatColor.YELLOW + Config.host + ":" + Config.port + "/" + Config.database, programCode );
         if ( p == null ) {
-            Tools.Prt( p, ChatColor.WHITE + "DB UserName : " + ChatColor.YELLOW + Config.username, programCode );
-            Tools.Prt( p, ChatColor.WHITE + "DB Password : " + ChatColor.YELLOW + Config.password, programCode );
+            Tools.Prt( p, ChatColor.WHITE + "DB UserName : " + ChatColor.YELLOW + Config.username + " [" + Config.password + "]", programCode );
         }
         Tools.Prt( p, ChatColor.WHITE + "Dynmap Icon : " + ChatColor.YELLOW + ( Config.OnDynmap ? "True":"False" ), programCode );
         Tools.Prt( p, ChatColor.WHITE + "LIKE : " + ChatColor.YELLOW + Config.like + 
-            " - UNLIKE : " + ChatColor.YELLOW + Config.unlike, programCode );
+            ChatColor.WHITE + " - UNLIKE : " + ChatColor.YELLOW + Config.unlike, programCode );
         Tools.Prt( p, ChatColor.WHITE + "SignSetKey  : " + ChatColor.YELLOW + Config.SignSetKey, programCode );
         Tools.Prt( p, ChatColor.WHITE + "Admin Name  : " + ChatColor.YELLOW + Config.AdminName, programCode );
         Tools.Prt( p, ChatColor.WHITE + "Inventory   : " + ChatColor.YELLOW + Config.InventoryTitle, programCode );
         Tools.Prt( p, ChatColor.WHITE + "Player Head : " + ChatColor.YELLOW + ( Config.MakeHead ? "Make" : "Plain" ) + " Icon", programCode );
         
-        Tools.Prt( p, ChatColor.WHITE + "Broadcast   : Link(" + ChatColor.YELLOW + ( Config.LikeBroadcast ? "Yes" : "No" ) +
-                ") Unlike(" + ChatColor.YELLOW + ( Config.UnlikeBroadcast ? "Yes" : "No" ) + ")", programCode );
-
-        Tools.Prt( p, ChatColor.WHITE + "Remove Sign Switch Lore", programCode );
-        Config.RemoveSignLore.stream().forEach( CP -> { Tools.Prt( p, ChatColor.WHITE + " - " + ChatColor.YELLOW + CP, programCode ); } );
-
-        Tools.Prt( p, ChatColor.WHITE + "This Like Sign Format", programCode );
-        Config.SignBase.stream().forEach( CP -> { Tools.Prt( p, ChatColor.WHITE + " - " + ChatColor.YELLOW + CP, programCode ); } );
+        Tools.Prt( p, ChatColor.WHITE + "Broadcast : Link(" +
+            ChatColor.YELLOW + ( Config.LikeBroadcast ? "Yes" : "No" ) +
+            ChatColor.WHITE + ") Unlike(" +
+            ChatColor.YELLOW + ( Config.UnlikeBroadcast ? "Yes" : "No" ) +
+            ChatColor.WHITE + ")", programCode
+        );
 
         if ( Reward.Flag ) {
             Tools.Prt( p, ChatColor.GREEN + "ThisLike Rewards", programCode );
             if ( Reward.sound_play ) {
                 Tools.Prt( p, ChatColor.WHITE + "Sound Type : " + ChatColor.YELLOW + Reward.sound_type +
-                    "V:" + ChatColor.YELLOW + Reward.sound_volume +
-                    " P:" + ChatColor.YELLOW + Reward.sound_pitch, programCode );
+                    ChatColor.WHITE + " V:" + ChatColor.YELLOW + Reward.sound_volume +
+                    ChatColor.WHITE + " P:" + ChatColor.YELLOW + Reward.sound_pitch, programCode );
             }
-            Tools.Prt( p, ChatColor.WHITE + "Date Count    : " + ChatColor.YELLOW + Reward.DateCount + " Days", programCode );
-            Tools.Prt( p, ChatColor.WHITE + "Magnification : " + ChatColor.YELLOW + Reward.magnification + " times", programCode );
+            Tools.Prt( p,
+                ChatColor.WHITE + "Date Count : " +
+                ChatColor.YELLOW + Reward.DateCount + " Days " +
+                ChatColor.AQUA + "(×" + Reward.magnification + ")",
+                programCode
+            );
             Tools.Prt( p, ChatColor.WHITE + "Reward Command", programCode );
             Reward.Commands.stream().forEach( CP -> { Tools.Prt( p, ChatColor.WHITE + " - " + ChatColor.YELLOW + CP, programCode ); } );
         }
@@ -122,6 +123,12 @@ public class ConfigManager {
     }
     
     public static void Message( Player p ) {
+        Tools.Prt( p, ChatColor.WHITE + "Remove Sign Switch Lore", programCode );
+        Config.RemoveSignLore.stream().forEach( CP -> { Tools.Prt( p, ChatColor.WHITE + " - " + ChatColor.YELLOW + CP, programCode ); } );
+
+        Tools.Prt( p, ChatColor.WHITE + "This Like Sign Format", programCode );
+        Config.SignBase.stream().forEach( CP -> { Tools.Prt( p, ChatColor.WHITE + " - " + ChatColor.YELLOW + CP, programCode ); } );
+
         Tools.Prt( p, ChatColor.GREEN + "=== ThisLike Message ===", programCode );
         Tools.Prt( p, ChatColor.WHITE + "YourSignMessage     : " + ChatColor.YELLOW + Config.YourSign, programCode );
         Tools.Prt( p, ChatColor.WHITE + "PlayerMessageLike   : " + ChatColor.YELLOW + Config.SetLike, programCode );
